5ca4605798629a9f92615893f8dfcc5bb6fb305b
Karanjot Bassi <karanjot.bassi@ucalgary.ca>
2023-11-29 23:24:14 -0700

    Added in the main pages for the student.

--- src/app.js
+++ src/app.js
@@ -8,8 +8,33 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/signin.html'));
+})
+
+app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/index.html'));
 })
+
+app.get('/settings', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/settings.html'));
+})
+
+app.get('/equipment', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/equipment.html'));
+})
+
+app.get('/book', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/book.html'));
+})
+
+app.get('/programs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/programs.html'));
+})
+
+app.get('/adminsignin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/adminsignin.html'));
+})
+
 
 const port = process.env.PORT || 3000;
 
