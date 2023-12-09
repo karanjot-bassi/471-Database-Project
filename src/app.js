c29e4a2531b6658c5f7eb6a01ec56bb0801440c8
Karanjot Bassi <karanjot.bassi@ucalgary.ca>
2023-12-09 00:49:38 -0700

    Fixed pages and styles

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ var connection = mysql.createConnection({
     host: 'localhost',
     database: 'UniSports',
     user: 'root',
-    //password: 'Uniting481fall'
-    password:'marwane123'
+    password: 'Uniting481fall'
+    //password:'marwane123'
 });
 
 
--- src/db.js
+++ src/db.js
@@ -4,8 +4,8 @@ var connection = mysql.createConnection({
     host: 'localhost',
     database: 'unisports',
     user: 'root',
-    //password: 'Uniting481fall'
-    password:'marwane123'
+    password: 'Uniting481fall'
+    //password:'marwane123'
 });
 
 
